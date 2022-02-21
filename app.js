fcd0bfeab3c302df4f3d1a2de54a95ee5c8875a2
Moe set <moeset89.myanmar@gmail.com>
2022-02-21 16:56:44 +0100

    Updated

---
+++ app.js
@@ -0,0 +1,41 @@
+
+const phrases = [
+    "I am studuent",
+    "Teamtree house",
+    "FrontEnd developer",
+    "JavaScript",
+    "TechDegree",
+]
+const qwerty = document.getElementById('qwerty');
+const Phrase = document.getElementById('phrase');
+const startGame = document.querySelector('.btn_reset');
+const overlay =  document.getElementById("overlay")
+const ul = document.querySelector('ul');
+let missed = 0;
+
+startGame.addEventListener('click', () =>{
+    overlay.style.display = "none";
+});
+function getRandomPhraseAsArray(arr) {
+    let randomNumber =  Math.floor(Math.random() * arr.length);
+    let randomPhrase = randomNumber[arr].tolowercase()
+    let randomSplit = randomPhrase.split('');
+    console.log(randomSplit);
+    return randomSplit;
+};
+const PhraseArray = getRandomPhraseAsArray(phrases);
+
+function addPhraseToDisplay (arr) {
+    for (let i=0; i<arr.length; i++) {
+        let li = document.createElement('li');
+        li.textContent =arr[i];
+        ul.appendChild(li);
+        
+        if(arr[i]===''){
+            li.className = '';
+        }else{
+            li.className = 'letter';
+        }
+    }
+};
+addPhraseToDisplay(PhraseArray);
